1b7f844584aab0da1329ecbec39429a9196a29c1
bgodfrey4 <bgodfrey94@gmail.com>
2022-05-20 13:42:20 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-int main (){
+/**
+ * main-sentence
+ *
+ * Return:sentence
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
