2b5d1ce8d709f18de9d37cf7a9bcd4d6dfd44813
bgodfrey4 <bgodfrey94@gmail.com>
2022-05-25 19:40:23 -0700

    ''corrections''

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints the alphabet in lowercase,
  * followed by a new line, except q and e
@@ -15,7 +14,6 @@
 	putchar(ch);
 	ch++;
 }
-	
 	putchar('\n');
 	return (0);
 }
