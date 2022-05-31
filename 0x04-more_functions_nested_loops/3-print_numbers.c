9f392c2842fbac244c1e3b72ec79cde07c17ae33
bgodfrey4 <bgodfrey94@gmail.com>
2022-05-31 12:37:23 -0700

    ''answers''

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ *
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+}
