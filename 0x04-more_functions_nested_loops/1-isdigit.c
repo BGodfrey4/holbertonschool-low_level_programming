3211a6efe6726392410e3cc7bb7f1fd7b85d2a79
bgodfrey4 <bgodfrey94@gmail.com>
2022-05-31 08:51:19 -0700

    ''answers''

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+* _isdigit - check code
+* @c: N/A
+* Return: #1 or 0 (Success)
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+	else
+	return (0);
+}
