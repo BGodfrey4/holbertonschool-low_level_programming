00e45830d8ec9bcbbb8bf5c13a674575d643439c
bgodfrey4 <bgodfrey94@gmail.com>
2022-06-02 21:58:06 -0700

    ''answers''

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* puts_half - prints the back half of string
+*
+* @v: int
+*
+* Return: void (success)
+*/
+
+void puts_half(char *str)
+{
+	int v = 0;
+
+	while (str[v] != '\n' && str[v] != '\0')
+{
+	v++;
+}
+	for (
+}
+	_putchar('\n');
+}
