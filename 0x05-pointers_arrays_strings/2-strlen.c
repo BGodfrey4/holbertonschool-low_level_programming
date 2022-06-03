3e67f08357ce1fcd7b1428284a73556812972e6d
bgodfrey4 <bgodfrey94@gmail.com>
2022-06-02 20:46:02 -0700

    ''answers''

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - will give the length of a string
+*
+* @i: is the given string to count
+*
+* Return: is the length of string
+*/
+
+int _strlen(char *i)
+{
+	int length = 0;
+
+	while
+	(i[length] != '\0') length++;
+	return (length);
+}
