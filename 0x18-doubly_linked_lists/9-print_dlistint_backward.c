a819c3b20576bdcf9fa29febcaf0269c49a814b6
bgodfrey4 <bgodfrey94@gmail.com>
2022-08-03 23:35:50 -0700

    answers

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -1,20 +1,26 @@
 #include "lists.h"
+
 /**
-* print_dlistint_backward - will print doubly linked list in reverse
-* @h: is the pointer to the head of the list
-*
-* Return: void
+* print_dlistint_backward - will print elements of dlistint_t list backwards
+* @h: is just the list to print
+* Return: is just the number of nodes
 */
 size_t print_dlistint_backward(const dlistint_t *h)
 {
-	size_t i = 0;
+	size_t numNodes = 0;
 
-	while (h->prev)
-	{
-		printf("%d\n", h->prev->n);
-		h = h->prev;
-		i++;
-	}
+	if (!h)
+	return (0);
+
+	while (h->next)
+	h = h->next;
+
+	while (h)
+{
 	printf("%d\n", h->n);
-	return (i + 1);
+	h = h->prev;
+	numNodes++;
+}
+
+	return (numNodes);
 }
