6088d62cd2396b9c19d4cf6c275f3b7581be1388
bgodfrey4 <bgodfrey94@gmail.com>
2022-08-03 22:00:43 -0700

    answers

--- 0x18-doubly_linked_lists/9-print_dlistint_backward.c
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -1,44 +1,20 @@
 #include "lists.h"
-
 /**
-* insert_dnodeint_at_index - inserts a node at a given index
-* in a doubly linked list
-* @h: double pointer to the list
-* @idx: index of the node to insert
-* @n: data to insert
-* Return: address of the new node, or NULL if it failed
+* print_dlistint_backward - will print doubly linked list in reverse
+* @h: is the pointer to the head of the list
+*
+* Return: void
 */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+size_t print_dlistint_backward(const dlistint_t *h)
 {
-	unsigned int i;
-	dlistint_t *new;
-	dlistint_t *temp = *h;
+	size_t i = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new || !h)
-	return (NULL);
-
-	new->n = n;
-	new->next = NULL;
-
-	if (idx == 0)
-	return (add_dnodeint(h, n));
-
-	for (i = 0; temp && i < idx; i++)
-{
-	if (i == idx - 1)
-{
-	if (temp->next == NULL)
-	return (add_dnodeint_end(h, n));
-	new->next = temp->next;
-	new->prev = temp;
-	temp->next->prev = new;
-	temp->next = new;
-	return (new);
-}
-	else
-	temp = temp->next;
-}
-
-	return (NULL);
+	while (h->prev)
+	{
+		printf("%d\n", h->prev->n);
+		h = h->prev;
+		i++;
+	}
+	printf("%d\n", h->n);
+	return (i + 1);
 }
